5319b776bb299c3d108d22d3408a388b34d30ce3
Schelokov Igor <topko2002@mail.ru>
2022-05-28 18:32:22 +1000

    project init

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace WInLogsViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msgTextBox = new System.Windows.Forms.RichTextBox();
+            this.table = new System.Windows.Forms.ListView();
+            this.Date = new System.Windows.Forms.ColumnHeader();
+            this.Type = new System.Windows.Forms.ColumnHeader();
+            this.Creator = new System.Windows.Forms.ColumnHeader();
+            this.Category = new System.Windows.Forms.ColumnHeader();
+            this.explorer = new System.Windows.Forms.ListView();
+            this.Log = new System.Windows.Forms.ColumnHeader();
+            this.Amount = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // msgTextBox
+            // 
+            this.msgTextBox.Location = new System.Drawing.Point(430, 541);
+            this.msgTextBox.Name = "msgTextBox";
+            this.msgTextBox.Size = new System.Drawing.Size(853, 165);
+            this.msgTextBox.TabIndex = 1;
+            this.msgTextBox.Text = "";
+            // 
+            // table
+            // 
+            this.table.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Date,
+            this.Type,
+            this.Creator,
+            this.Category});
+            this.table.FullRowSelect = true;
+            this.table.GridLines = true;
+            this.table.HideSelection = false;
+            this.table.Location = new System.Drawing.Point(430, 3);
+            this.table.MultiSelect = false;
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(862, 532);
+            this.table.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.table.TabIndex = 2;
+            this.table.UseCompatibleStateImageBehavior = false;
+            this.table.View = System.Windows.Forms.View.Details;
+            this.table.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.filterLog);
+            this.table.MouseClick += new System.Windows.Forms.MouseEventHandler(this.showMessage);
+            // 
+            // Date
+            // 
+            this.Date.Text = "Дата";
+            this.Date.Width = table.Width / 4;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Тип";
+            this.Type.Width = table.Width / 4;
+            // 
+            // Creator
+            // 
+            this.Creator.Text = "Создатель";
+            this.Creator.Width = table.Width / 4;
+            // 
+            // Category
+            // 
+            this.Category.Text = "Категория";
+            this.Category.Width = table.Width / 4;
+            // 
+            // explorer
+            // 
+            this.explorer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Log,
+            this.Amount});
+            this.explorer.FullRowSelect = true;
+            this.explorer.GridLines = true;
+            this.explorer.HideSelection = false;
+            this.explorer.Location = new System.Drawing.Point(3, 3);
+            this.explorer.MultiSelect = false;
+            this.explorer.Name = "explorer";
+            this.explorer.Size = new System.Drawing.Size(421, 703);
+            this.explorer.TabIndex = 3;
+            this.explorer.UseCompatibleStateImageBehavior = false;
+            this.explorer.View = System.Windows.Forms.View.Details;
+            this.explorer.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.chooseLog);
+            // 
+            // Log
+            // 
+            this.Log.Text = "Лог";
+            this.Log.Width = 3 * (explorer.Width / 4);
+            // 
+            // Amount
+            // 
+            this.Amount.Text = "Количество";
+            this.Amount.Width = (explorer.Width / 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 718);
+            this.Controls.Add(this.explorer);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.msgTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox msgTextBox;
+        private System.Windows.Forms.ListView table;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader Creator;
+        private System.Windows.Forms.ColumnHeader Category;
+        private System.Windows.Forms.ListView explorer;
+        private System.Windows.Forms.ColumnHeader Log;
+        private System.Windows.Forms.ColumnHeader Amount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WInLogsViewer
+{
+    public partial class Form1 : Form
+    {
+        public EventLog[] eventLogs;
+        public EventLogEntryCollection entries;
+        int currentJournal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            eventLogs = EventLog.GetEventLogs();
+
+            foreach (EventLog log in eventLogs)
+            {
+                var row = new ListViewItem(new[] { log.Log.ToString(), log.Entries.Count.ToString() });
+                explorer.Items.Add(row);
+            }
+        }
+
+        private void filterLog(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == 0)
+            {
+                EventLogEntry[] sorted = new EventLogEntry[entries.Count];
+                table.Items.Clear();
+                entries.CopyTo(sorted, 0);
+                //Quicksort(sorted, 0, sorted.Length - 1);
+                sorted = BubbleSort(sorted);
+
+                foreach (EventLogEntry j in sorted)
+                {
+                    var row = new ListViewItem(new[] {j.TimeWritten.ToString(),
+                                                      j.EntryType.ToString(),
+                                                      j.Source,
+                                                      j.Category});
+                    table.Items.Add(row);
+                }
+            }
+            else if (e.Column == 1)
+            {
+                EventLogEntry[] sorted = new EventLogEntry[entries.Count];
+                entries.CopyTo(sorted, 0);
+                table.Items.Clear();
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void chooseLog(object sender, MouseEventArgs e)
+        {
+            table.Items.Clear();
+            for (int i = 0; i < explorer.Items.Count; i++)
+            {
+                var rectangle = explorer.GetItemRect(i);
+                if (rectangle.Contains(e.Location))
+                {
+                    entries = eventLogs[i].Entries;
+                    currentJournal = i;
+                    for (int j = 0; j < entries.Count; j++)
+                    {
+                        var row = new ListViewItem(new[] {entries[j].TimeWritten.ToString(),
+                                                          entries[j].EntryType.ToString(),
+                                                          entries[j].Source,
+                                                          entries[j].Category});
+                        table.Items.Add(row);
+                    }
+                    return;
+                }
+            }
+        }
+
+        private void showMessage(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < table.Items.Count; i++)
+            {
+                var rectangle = table.GetItemRect(i);
+                if (rectangle.Contains(e.Location))
+                {
+                    msgTextBox.Text = entries[i].Message.ToString();
+                    return;
+                }
+            }
+        }
+
+        int Partition(EventLogEntry[] array, int start, int end)
+        {
+            int marker = start; // divides left and right subarrays
+            for (int i = start; i < end; i++)
+            {
+                if (DateTime.Compare(array[i].TimeWritten, array[end].TimeWritten) < 0) // array[end] is pivot
+                {
+                    (array[marker], array[i]) = (array[i], array[marker]);
+                    marker++;
+                }
+            }
+            // put pivot(array[end]) between left and right subarrays
+            (array[marker], array[end]) = (array[end], array[marker]);
+            return marker;
+        }
+
+        void Quicksort(EventLogEntry[] array, int start, int end)
+        {
+            if (start >= end)
+                return;
+
+            int pivot = Partition(array, start, end);
+            Quicksort(array, start, pivot - 1);
+            Quicksort(array, pivot + 1, end);
+        }
+
+        static EventLogEntry[] BubbleSort(EventLogEntry[] mas)
+        {
+            EventLogEntry temp;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (DateTime.Compare(mas[i].TimeWritten, mas[j].TimeWritten) > 0)
+                    {
+                        temp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = temp;
+                    }
+                }
+            }
+            return mas;
+        }
+    }
+
+}
